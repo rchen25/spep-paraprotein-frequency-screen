623010605a4f6203ea79e8c83cec1350f521d7cc
User 1 <user1@Users-MacBook-Air.local>
2022-02-08 14:13:18 -0500

    allow input to be in integer array form

--- paraprotein_screen.py
+++ paraprotein_screen.py
@@ -12,36 +12,69 @@ from model import HighFreqScreener
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("infile", help="input file with serum samples and control curves")
+	parser.add_argument("--input_int", help="if set to 1, serum and control samples are input in integer format; \
+										   note: gamma region cutoff is required input")
 	args = parser.parse_args()
 
 	df_input_samples_to_screen = pd.read_csv(args.infile)
 
-
-
-	# print('Paraprotein screening tool ... ')
-	# print('input filename:', args.infile)
-	# print('number of samples to screen: ', len(df_input_samples_to_screen))
-
+	if args.input_int:
+		if int(args.input_int) == 1:
+			input_is_int = 1
+		elif int(args.input_int) == 0:
+			input_is_int = 0
+		else:
+			input_is_int = 1 # set as 1 by default if user entered a non-zero value 
+	else:
+		input_is_int = 0
 
 	# data processing #################################################################################################
+	if not input_is_int:
+		## convert curves from hex format to int array
+		l_curves_features = ['sebiaSerumCurve', 'sebiaSerumGelControlCurve']
 
-	## convert curves from hex format to int array
-	l_curves_features = ['sebiaSerumCurve', 'sebiaSerumGelControlCurve']
+		for feature_name in l_curves_features:
+		    df_input_samples_to_screen[feature_name + '_intArr'] = [DataProcessing.convert_hex_string_to_float_array_impute_landmarks(x)[0] for 
+		                                                 x in df_input_samples_to_screen[feature_name]]
 
-	for feature_name in l_curves_features:
-	    df_input_samples_to_screen[feature_name + '_intArr'] = [DataProcessing.convert_hex_string_to_float_array_impute_landmarks(x)[0] for 
-	                                                 x in df_input_samples_to_screen[feature_name]]
+		    
+		    df_input_samples_to_screen[feature_name + '_landmarks'] = [DataProcessing.convert_hex_string_to_float_array_impute_landmarks(x)[1] for 
+		                                                 x in df_input_samples_to_screen[feature_name]]
 
-	    
-	    df_input_samples_to_screen[feature_name + '_landmarks'] = [DataProcessing.convert_hex_string_to_float_array_impute_landmarks(x)[1] for 
-	                                                 x in df_input_samples_to_screen[feature_name]]
+		## compute gamma cutoffs 
+		ll_gamma_landmarks = [x for x in df_input_samples_to_screen['sebiaSerumGelControlCurve_landmarks']]
 
+		### determine the X-axis cutoffs for the gamma region:
+		gamma_cutoffs_X = np.zeros([df_input_samples_to_screen.shape[0]])
+
+		cnt = 0
+		for landmarks_this_patient in ll_gamma_landmarks:
+		    list_of_C_8 = [k for k in landmarks_this_patient if k[1] == 'C' or k[1] == '8']
+		    gamma_cutoff_this_pt = list_of_C_8[1][0]
+		    gamma_cutoffs_X[cnt] = gamma_cutoff_this_pt
+		    cnt += 1
+		### impute if not found
+		gamma_cutoffs_X[gamma_cutoffs_X == 0] = np.mean(gamma_cutoffs_X[gamma_cutoffs_X != 0])
+
+		del df_input_samples_to_screen['sebiaSerumGelControlCurve_landmarks']
+		del df_input_samples_to_screen['sebiaSerumCurve_landmarks']
+
+	else:
+		df_input_samples_to_screen['sebiaSerumCurve_intArr'] = [k.strip('[').strip(']').split(',') for k in df_input_samples_to_screen['sebiaSerumCurve_intArr']]
+		df_input_samples_to_screen['sebiaSerumCurve_intArr'] = [np.array(x).astype(int) for x in df_input_samples_to_screen['sebiaSerumCurve_intArr']]
+
+		df_input_samples_to_screen['sebiaSerumGelControlCurve_intArr'] = [k.strip('[').strip(']').split(',') for k in df_input_samples_to_screen['sebiaSerumGelControlCurve_intArr']]
+		df_input_samples_to_screen['sebiaSerumGelControlCurve_intArr'] = [np.array(x).astype(int) for x in df_input_samples_to_screen['sebiaSerumGelControlCurve_intArr']]
+
+		gamma_cutoffs_X = np.array(df_input_samples_to_screen['gamma_region_cutoff'])
+		### impute any invalid cutoffs
+		gamma_cutoffs_X[(gamma_cutoffs_X != gamma_cutoffs_X) | (gamma_cutoffs_X==0)] = np.mean(gamma_cutoffs_X[gamma_cutoffs_X != 0])
 
 
 	## create feature matrix
 	ll_serum_curve = [x for x in df_input_samples_to_screen['sebiaSerumCurve_intArr']]
 	ll_serum_gel_control_curve = [x for x in df_input_samples_to_screen['sebiaSerumGelControlCurve_intArr']]
-	ll_gamma_landmarks = [x for x in df_input_samples_to_screen['sebiaSerumGelControlCurve_landmarks']]
+	
 
 	### serum and control curves in numeric form
 	X_serum_curve = np.array(ll_serum_curve)
@@ -55,18 +88,6 @@ def main():
 	### serum curve - control curve difference
 	X_serum_minus_control = X_serum_curve - X_serum_control_curve
 
-	## compute gamma cutoffs
-	### determine the X-axis cutoffs for the gamma region:
-	gamma_cutoffs_X = np.zeros([X_serum_minus_control.shape[0]])
-
-	cnt = 0
-	for landmarks_this_patient in ll_gamma_landmarks:
-	    list_of_C_8 = [k for k in landmarks_this_patient if k[1] == 'C' or k[1] == '8']
-	    gamma_cutoff_this_pt = list_of_C_8[1][0]
-	    gamma_cutoffs_X[cnt] = gamma_cutoff_this_pt
-	    cnt += 1
-	### impute if not found
-	gamma_cutoffs_X[gamma_cutoffs_X == 0] = np.mean(gamma_cutoffs_X[gamma_cutoffs_X != 0])
 
 
 	## zero out non-gamma fractions (different location cutoff on gel per sample)
@@ -87,8 +108,7 @@ def main():
 	y_class = (y_proba > model_highpass.threshold_for_likelihood_ratio_90).astype(int)
 
 	df_output = df_input_samples_to_screen.copy(deep=True)
-	del df_output['sebiaSerumGelControlCurve_landmarks']
-	del df_output['sebiaSerumCurve_landmarks']
+
 	df_output['sebiaSerumCurve_intArr'] = [np.array2string(x, separator=',').replace('\n', '') for x in df_output['sebiaSerumCurve_intArr']]
 	df_output['sebiaSerumGelControlCurve_intArr'] = [np.array2string(x, separator=',').replace('\n', '') for x in df_output['sebiaSerumGelControlCurve_intArr']]
 	df_output['gamma_region_cutoff'] = gamma_cutoffs_X
@@ -98,6 +118,7 @@ def main():
 	df_output.to_csv('./output.csv', header = True, index = False)
 	df_output.to_json('./output.json', orient = 'records')
 	print(df_output.to_json(orient = 'records'))
+
 
 if __name__ == "__main__":
 	main()
