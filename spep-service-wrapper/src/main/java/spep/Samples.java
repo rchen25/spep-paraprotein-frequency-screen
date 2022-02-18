273a42a2b294e9410336f7b175ba4218f3d51905
Geoffrey H. Smith <geoffrey.hughes.smith@gmail.com>
2022-02-18 16:03:06 -0500

    added GET method with {sampleCurve}/{controlCurve} in URL path
    This provides an alternative to the POST method, which can be cumbersome to use from functionally limited clients (e.g., Microsoft Excel).

--- spep-service-wrapper/src/main/java/spep/Samples.java
+++ spep-service-wrapper/src/main/java/spep/Samples.java
@@ -15,6 +15,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 @Path("samples")
@@ -47,6 +48,20 @@ public class Samples {
 
     }
 
+    @GET
+    @Path("{sampleCurve}/{controlCurve}")
+    @Produces("application/json")
+    public Sample getJson(@PathParam("sampleCurve") String sampleCurve, @PathParam("controlCurve") String controlCurve) throws IOException, InterruptedException {
+
+        Sample sample = new Sample();
+        sample.sebiaSerumCurveHex = sampleCurve;
+        sample.sebiaSerumGelControlCurveHex = controlCurve;
+        sample = postJson(sample);
+
+        return sample;
+
+    }
+    
     @POST
     @Consumes("application/json")
     @Produces("application/json")
